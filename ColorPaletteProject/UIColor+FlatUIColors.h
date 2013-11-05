e250d8035042fbd5e10072e5d6eeaaa4d2126e82
Michael MacCallum <maccdevteam@gmail.com>
2013-11-05 07:39:44 -0500

    Declarations done and examples added. Example merely changes the background color of the view controller’s view every 1.5 seconds to the next color in the category. These are just UIColor objects, so they shouldn’t need a detailed explanation of how to use them.

--- ColorPaletteProject/UIColor+FlatUIColors.h
+++ ColorPaletteProject/UIColor+FlatUIColors.h
@@ -10,4 +10,145 @@
 
 @interface UIColor (FlatUIColors)
 
+/**
+ *  From UIColor+FlatUIColors Category rgba(26, 188, 156,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) turquoiseColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(46, 204, 113,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) emeraldcolor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(52, 152, 219,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) peterRiverColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(155, 89, 182,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) amethystColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(52, 73, 94,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) wetAsphaltColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(22, 160, 133,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) greenSeaColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(39, 174, 96,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) nephritisColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(41, 128, 185,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) belizeHoleColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(142, 68, 173,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) wisteriaColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(44, 62, 80,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) midnightBlueColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(241, 196, 15,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) sunFlowerColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(230, 126, 34,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) carrotColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(231, 76, 60,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) alizarinColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(236, 240, 241,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) cloudsColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(149, 165, 166,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) concreteColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(243, 156, 18,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) orange_Color;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(211, 84, 0,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) pumpkinColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(192, 57, 43,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) pomegranateColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(189, 195, 199,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) silverColor;
+
+/**
+ *  From UIColor+FlatUIColors Category rgba(127, 140, 141,1.0)
+ *
+ *  @return UIColor object with values
+ */
++ (UIColor *) asbestosColor;
+
+
 @end
--- ColorPaletteProject/ViewController.m
+++ ColorPaletteProject/ViewController.m
@@ -7,23 +7,31 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "UIColor+FlatUIColors.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self.view setBackgroundColor:[UIColor turquoiseColor]];
+    [NSTimer scheduledTimerWithTimeInterval:1.5 target:self selector:@selector(timerFired:) userInfo:nil repeats:YES];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)timerFired:(NSTimer *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSArray *arr = @[[UIColor turquoiseColor],[UIColor emeraldcolor],[UIColor peterRiverColor],[UIColor amethystColor],[UIColor wetAsphaltColor],[UIColor greenSeaColor],[UIColor nephritisColor],[UIColor belizeHoleColor],[UIColor wisteriaColor],[UIColor midnightBlueColor],[UIColor sunFlowerColor],[UIColor carrotColor],[UIColor alizarinColor],[UIColor cloudsColor],[UIColor concreteColor],[UIColor orange_Color],[UIColor pumpkinColor],[UIColor pomegranateColor],[UIColor silverColor],[UIColor asbestosColor]];
+
+    NSInteger idx = [arr indexOfObject:self.view.backgroundColor];
+
+    if (idx < arr.count - 1) {
+        [UIView animateWithDuration:0.25 animations:^{
+            [self.view setBackgroundColor:arr[idx + 1]];
+        }];
+    }
 }
+
+
 
 @end
